cc2357de9daea200c223428725d1280240294dc2
SusieHatter <susiivaldez@gmail.com>
2021-06-12 16:51:27 -0500

    Add house layout

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ kaboom({
 });
 
 // Constants
+const TILE_WIDTH = 64;
 const PLAYER_SPEED = 200;
 const OTHER_PLAYER_SPEED = 190;
 const GHOST_SPEED = 50;
@@ -20,81 +21,57 @@ loadRoot("images/");
   "dog",
   "ghost",
   "key",
-  "wood-wall",
-].forEach(spriteName =>
-  loadSprite(spriteName, spriteName + ".png")
-)
+  "floor",
+  "door-front",
+  "door-side",
+  "wood-wall-front",
+  "wood-wall-side",
+  "garden-wall",
+  "table",
+  "barrel",
+  "chair-front",
+  "bed",
+].forEach((spriteName) => loadSprite(spriteName, spriteName + ".png"));
 
 // Map
 const map = [
-  "####################################################################################################################",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                 k                                                                                                #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "#                                                                                                                  #",
-  "####################################################################################################################",
+  "++++++++++++++++++++++++++++++++++++++++",
+  "+                                      +",
+  "+                                      +",
+  "+                                      +",
+  "+                                      +",
+  "+                                      +",
+  "+                                      +",
+  "+                                      +",
+  "#---#-[-#----------------------#--[----#",
+  "#   #   #                      #       #",
+  "#   #   #                      #       #",
+  "#   #   #                      =       #",
+  "#   #   #                      #       #",
+  "#---#   #-----[----------------#-------#",
+  "#   #   #                      #       #",
+  "#   #   #                      =       #",
+  "#   #   #                      #       #",
+  "#   #   #                      #       #",
+  "#---#   #                      #       #",
+  "#   #   #                      #       #",
+  "#   #   #                      #       #",
+  "#   #   #                      #       #",
+  "#---#   #-----#----------#-----#       #",
+  "#bB #   #     #          #             #",
+  "#   #   #     #          #             #",
+  "#  c#   #                              #",
+  "#  t#   #                              #",
+  "#   =   #     #          #             #",
+  "#   #   #     #          #             #",
+  "-------------------[[-------------------",
 ];
-
 // Movement directions
 const dirs = {
-  "w": vec2(0, -1),
-  "s": vec2(0, 1),
-  "a": vec2(-1, 0),
-  "d": vec2(1, 0),
+  w: vec2(0, -1),
+  s: vec2(0, 1),
+  a: vec2(-1, 0),
+  d: vec2(1, 0),
 };
 
 // function closest(vec, vecs) {
@@ -103,54 +80,50 @@ const dirs = {
 //     return closest;
 // }
 
+layers(["floor", "objects"], "objects");
+
 scene("main", () => {
+  add([sprite("floor"), pos(0, 0)]);
   addLevel(map, {
-    width: 16,
-    height: 16,
-    "#": [
-      sprite("wood-wall"),
-      solid(),
-    ],
-    "k": [
-      sprite("key"),
-      area(vec2(-16), vec2(32)),
-      INTERACTABLE,
-    ]
+    width: TILE_WIDTH,
+    height: TILE_WIDTH,
+    "[": [sprite("door-front")],
+    "=": [sprite("door-side")],
+    "-": [sprite("wood-wall-front"), solid()],
+    "#": [sprite("wood-wall-side"), solid()],
+    "+": [sprite("garden-wall"), solid()],
+    b: [sprite("barrel"), solid()],
+    B: [sprite("bed"), solid()],
+    c: [sprite("chair-front"), solid()],
+    t: [sprite("table"), solid()],
+    k: [sprite("key"), area(vec2(-16), vec2(32)), INTERACTABLE],
   });
 
   const man = add([
     sprite("man"),
-    pos(100, 100),
+    pos(3 * TILE_WIDTH, 23 * TILE_WIDTH),
     solid(),
   ]);
 
   const dog = add([
     sprite("dog"),
-    pos(120, 100),
+    pos(1 * TILE_WIDTH, 24 * TILE_WIDTH),
     solid(),
   ]);
 
-  const ghost = add([
-    sprite("ghost"),
-    pos(180, 200),
-    solid(),
-  ])
+  const ghost = add([sprite("ghost"), pos(180, 200), solid()]);
 
   const state = {
     activePlayer: man,
     otherPlayer: dog,
     currentInteractable: undefined,
     beingCalled: false,
-  }
+  };
 
   // Player movement
   for (const dir in dirs) {
     keyDown(dir, () => {
-      state.activePlayer.move(
-        dirs[dir]
-          .unit()
-          .scale(PLAYER_SPEED)
-      );
+      state.activePlayer.move(dirs[dir].unit().scale(PLAYER_SPEED));
     });
   }
 
@@ -160,8 +133,7 @@ scene("main", () => {
   }
 
   keyPress("e", () => {
-    if (state.activePlayer !== man
-      || state.currentInteractable === undefined) {
+    if (state.activePlayer !== man || state.currentInteractable === undefined) {
       return;
     }
     interact(state.currentInteractable);
